e5f5844c8a32cc1e68ac0ee8245a46e69d6c5540
shinyarie <17.8moon@gmail.com>
2021-05-16 16:22:17 +0900

    itemのindexページを作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
-    
+    @total_items = Item.all
+    @items = Item.all.page(params[:page]).per(8)
   end
-  
+
   def show
-    
+
   end
 end
