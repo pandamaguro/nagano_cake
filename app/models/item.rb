d3a95fd772364b1518edab47b2a27d4853553069
kento-96 <kento.soccer.1107@gmail.com>
2021-05-16 05:47:42 +0000

    アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  has_many :order_details, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  belongs_to :genre
+  has_many :orders, through: :order_details
+
+
+  attachment :image
+  
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :genre_id, presence: true
+   
 end
