1c6e7e2829481012777ae5b87cd685ecd5f15bcf
kento-96 <kento.soccer.1107@gmail.com>
2021-05-16 07:56:44 +0000

    ジャンル一覧・新規作成ページ

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,7 @@
 class Admin::GenresController < ApplicationController
   def index
-    
+    @genres = Genre.all
+    @genre = Genre.new
   end
   
   def edit
@@ -8,10 +9,23 @@ class Admin::GenresController < ApplicationController
   end
   
   def create
+    @genre = Genre.new(genre_params)
+		if @genre.save
+		  redirect_to admin_genres_path
+		else
+		  @genres = Genre.all
+		  render :index
+		end
     
   end
   
   def update
     
   end
+  
+  private
+
+	def genre_params
+	  params.require(:genre).permit(:name)
+	end
 end
