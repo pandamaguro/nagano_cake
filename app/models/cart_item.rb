eede7bb92565b1a186108e5f109a7422fd85d43c
shinyarie <17.8moon@gmail.com>
2021-05-16 13:37:15 +0900

    アドレス、カートアイテム、オーダー、オーダー詳細のアソシエーションを記入

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,6 @@
 class CartItem < ApplicationRecord
+  belongs_to :customer
+  belongs_to :item
+	validates :customer_id, :item_id, :count, presence: true
+  validates :count, numericality: { only_integer: true }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+	has_many :order_details, dependent: :destroy
+
+	validates :customer_id, :address, :name, :shipping_cost,
+			  		:total_payment, :payment_method,
+			  		presence: true
+	validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
+	validates :shipping_cost, :total_payment, numericality: { only_integer: true }
+
+	enum payment_method: {"クレジットカード": 0,"銀行振込": 1}
+	enum status: {"入金待ち": 0,"入金確認": 1,"製作中": 2,"発送準備中": 3, "発送済み": 4}
 end
