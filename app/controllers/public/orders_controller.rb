0b53f856919571ff84109090c06d1bd7327043d3
shinyarie <17.8moon@gmail.com>
2021-05-18 08:55:42 +0900

    注文履歴一覧のページの作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,25 +1,26 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
   def index
-    
+    @orders = current_customer.orders
   end
-  
+
   def show
-    
+
   end
-  
+
   def new
-    
+
   end
-  
+
   def confirm
-    
+
   end
-  
+
   def create
-  
+
   end
-  
+
   def thanx
-    
-  end  
+
+  end
 end
