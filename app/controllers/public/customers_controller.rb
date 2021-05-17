b62694d7b39ba31b28590a5ddc2214f0375d7868
EC2 Default User <ec2-user@ip-172-31-82-5.ec2.internal>
2021-05-17 03:59:47 +0000

    会員情報編集ページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,10 +4,16 @@ class Public::CustomersController < ApplicationController
   end
   
   def edit 
-    
+    @customer = current_customer
   end
   
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_path
+    else
+      render :edit
+    end
     
   end
   
@@ -17,5 +23,12 @@ class Public::CustomersController < ApplicationController
   
   def out
     
-  end  
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(
+      :last_name,:first_name,:kana_last_name,:kana_first_name,:email,:postal_code,:address,:phone_number)
+  end
 end
