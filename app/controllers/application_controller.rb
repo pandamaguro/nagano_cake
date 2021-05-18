5cd8f7ac6379eee9282f9fd3dbf78ff984fa803f
shinyarie <17.8moon@gmail.com>
2021-05-19 01:19:48 +0900

    新規登録時に一部のパラメータがunpermitになっていたので、そのカラムをpermitに追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    
+    before_action :configure_permitted_parameters, if: :devise_controller?
      private
     def after_sign_in_path_for(resource)
       case resource
@@ -8,5 +8,10 @@ class ApplicationController < ActionController::Base
       when Customer
         root_path
       end
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :address, :phone_number])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
     end
 end
