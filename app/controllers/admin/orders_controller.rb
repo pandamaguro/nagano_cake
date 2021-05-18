fe9dc4264f2ed6d93426ac119dc4eb5ff801a679
kento-96 <kento.soccer.1107@gmail.com>
2021-05-18 12:05:17 +0000

    admin権限

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,5 @@
 class Admin::OrdersController < ApplicationController
+   before_action :authenticate_admin!
   def index
     
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    
+     private
+    def after_sign_in_path_for(resource)
+      case resource
+      when Admin
+        admin_items_path
+      when Customer
+        root_path
+      end
+    end
 end
