56a576dbb213bb1bbdb0df9e3f4b2748fb6f883b
kento-96 <kento.soccer.1107@gmail.com>
2021-05-18 11:41:02 +0000

    admin権限

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CustomersController < ApplicationController
+   before_action :authenticate_admin!
   def index
     @customers = Customer.all.page(params[:page]).per(10)
     
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ItemsController < ApplicationController
+   before_action :authenticate_admin!
   def index
     @items = Item.page(params[:page]).per(10)
   end
