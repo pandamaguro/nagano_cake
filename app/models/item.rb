ba0e33f021fa1bb15d409d89dbca0cd41d8989d4
shinyarie <17.8moon@gmail.com>
2021-05-20 17:01:45 +0900

    無駄な空行削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,11 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :orders, through: :order_details
 
-
   attachment :image
-  
+
   validates :name, presence: true
   validates :image, presence: true
   validates :introduction, presence: true
   validates :price, presence: true
   validates :genre_id, presence: true
-   
 end
