a6cf08cec5e835570ac2dbb3659428f66d18a292
EC2 Default User <ec2-user@ip-172-31-82-5.ec2.internal>
2021-05-20 06:44:28 +0000

    devise関係、ヘッダー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,21 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
      private
-    def after_sign_in_path_for(resource)
-      case resource
-      when Admin
-        admin_items_path
-      when Customer
+    def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+        admin_orders_path
+      else
         root_path
       end
     end
+    
+    def after_sign_out_path_for(resource_or_scope)
+      if  resource_or_scope == :admin
+        new_admin_session_path
+      else
+        root_path
+      end
+    end    
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :address, :phone_number])
